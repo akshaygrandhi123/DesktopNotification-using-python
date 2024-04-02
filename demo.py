2582308c3b0e08aca838f4eeb6fa0b2b0fdf578e
akshaygrandhi123 <akshaygrandhi123@gmail.com>
2024-04-03 00:30:41 +0530

    Add demo.py

---
+++ demo.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import os
+
+def restart():
+    os.system("shutdown /r /t 1")
+
+def restart_time():
+    os.system("shutdown /r /t 20")
+
+def logout():
+    os.system("shutdown -l")
+
+def shutdown():
+    os.system("shutdown /s /t 1")
+
+st = Tk()
+st.title("Shutdown")
+st.geometry("500x500")
+st.config(bg="blue")
+
+r_button = Button(st, text="Restart", font=("Times New Roman", 20, "bold"), relief=RAISED, cursor="plus", command=restart)
+r_button.place(x=150, y=60, height=50, width=200)
+
+r_button = Button(st, text="Restart Time", font=("Times New Roman", 20, "bold"), relief=RAISED, cursor="plus", command=restart_time)
+r_button.place(x=150, y=170, height=50, width=200)
+
+lg_button = Button(st, text="Log-Out", font=("Times New Roman", 20, "bold"), relief=RAISED, cursor="plus", command=logout)
+lg_button.place(x=150, y=270, height=50, width=200)
+
+st_button = Button(st, text="Shutdown", font=("Times New Roman", 20, "bold"), relief=RAISED, cursor="plus", command=shutdown)
+st_button.place(x=150, y=370, height=50, width=200)
+
+st.mainloop()
